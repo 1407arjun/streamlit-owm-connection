df301bacc8597404fc94687a398d42e2ce859780
Arjun Sivaraman <arjun140702@gmail.com>
2023-07-31 16:57:18 -0700

    Format all files

--- demo/st_owm_connection/connection.py
+++ demo/st_owm_connection/connection.py
@@ -9,6 +9,7 @@ from datetime import timedelta
 BASE_URL = "https://api.openweathermap.org/data/2.5"
 PRO_URL = "https://pro.openweathermap.org/data/2.5"
 
+
 class OpenWeatherMapConnection(ExperimentalBaseConnection[Session]):
     __appid: str | None
     units: Literal["standard", "metric", "imperial"] = "standard"
@@ -32,26 +33,26 @@ class OpenWeatherMapConnection(ExperimentalBaseConnection[Session]):
 
         if 'lang' in kwargs:
             self.lang = kwargs.pop('lang')
-        
+
         self.session = requests.Session()
         return self.session
-    
+
     # Private method to get the AppID within the conncection class
     def __get_appid(self) -> str:
         return self.__appid
-    
+
     # Reset and return a new connection with the same AppID
     def reset(self):
         self.session.close()
         self.session = requests.Session()
-    
+
     # Setters for units and language preferences
     def set_units(self, units: Literal["standard", "metric", "imperial"]):
         self.units = units
 
     def set_lang(self, lang: str):
         self.lang = lang
-    
+
     # Call current weather data
     def current(self, q: str = None, lat: float = None, lon: float = None, id: int = None, zip: str = None, units: Literal["standard", "metric", "imperial"] = None, lang: str = None, ttl: Optional[Union[float, int, timedelta]] = None) -> Any:
         units = units if units else self.units
@@ -59,22 +60,26 @@ class OpenWeatherMapConnection(ExperimentalBaseConnection[Session]):
 
         @cache_data(ttl=ttl, show_spinner=f"Loading current weather for {q}...")
         def query(q: str):
-            r = self.session.get(f"{BASE_URL}/weather?q={q}&appid={self.__get_appid()}&units={units}&lang={lang}")
+            r = self.session.get(
+                f"{BASE_URL}/weather?q={q}&appid={self.__get_appid()}&units={units}&lang={lang}")
             return r.json()
-        
+
         @cache_data(ttl=ttl, show_spinner=f"Loading current weather for {lat}, {lon}...")
         def latlon(lat: float, lon: float):
-            r = self.session.get(f"{BASE_URL}/weather?lat={lat}&lon={lon}&appid={self.__get_appid()}&units={units}&lang={lang}")
+            r = self.session.get(
+                f"{BASE_URL}/weather?lat={lat}&lon={lon}&appid={self.__get_appid()}&units={units}&lang={lang}")
             return r.json()
-        
+
         @cache_data(ttl=ttl, show_spinner=f"Loading current weather for {zip}...")
         def cityid(id: str):
-            r = self.session.get(f"{BASE_URL}/weather?id={id}&appid={self.__get_appid()}&units={units}&lang={lang}")
+            r = self.session.get(
+                f"{BASE_URL}/weather?id={id}&appid={self.__get_appid()}&units={units}&lang={lang}")
             return r.json()
-        
+
         @cache_data(ttl=ttl, show_spinner=f"Loading current weather for {zip}...")
         def zipcode(zip: str):
-            r = self.session.get(f"{BASE_URL}/weather?zip={zip}&appid={self.__get_appid()}&units={units}&lang={lang}")
+            r = self.session.get(
+                f"{BASE_URL}/weather?zip={zip}&appid={self.__get_appid()}&units={units}&lang={lang}")
             return r.json()
 
         if q is not None:
@@ -87,7 +92,7 @@ class OpenWeatherMapConnection(ExperimentalBaseConnection[Session]):
             return zipcode(zip=zip)
         else:
             return None
-    
+
     # Call 5 day / 3 hour, 16 day / daily, hourly forecast data or climate forecast for 30 days
     def forecast(self, type: Literal["3hr", "daily", "hourly", "climate"] = "3hr", q: str = None, lat: float = None, lon: float = None, id: int = None, zip: str = None, cnt: int = None, units: Literal["standard", "metric", "imperial"] = None, lang: str = None, ttl: Optional[Union[float, int, timedelta]] = None) -> Any:
         units = units if units else self.units
@@ -95,26 +100,31 @@ class OpenWeatherMapConnection(ExperimentalBaseConnection[Session]):
         count = "&cnt=" + cnt if cnt else ""
 
         type = "" if type == "3hr" else type
-        ENDPOINT = PRO_URL if type in ["daily", "hourly", "climate"] else BASE_URL
+        ENDPOINT = PRO_URL if type in [
+            "daily", "hourly", "climate"] else BASE_URL
 
         @cache_data(ttl=ttl, show_spinner=f"Loading {type} weather forecast for {q}...")
         def query(q: str):
-            r = self.session.get(f"{ENDPOINT}/forecast/{type}?q={q}&appid={self.__get_appid()}&units={units}&lang={lang}{count}")
+            r = self.session.get(
+                f"{ENDPOINT}/forecast/{type}?q={q}&appid={self.__get_appid()}&units={units}&lang={lang}{count}")
             return r.json()
-        
+
         @cache_data(ttl=ttl, show_spinner=f"Loading {type} weather forecast for {lat}, {lon}...")
         def latlon(lat: float, lon: float):
-            r = self.session.get(f"{ENDPOINT}/forecast/{type}?lat={lat}&lon={lon}&appid={self.__get_appid()}&units={units}&lang={lang}{count}")
+            r = self.session.get(
+                f"{ENDPOINT}/forecast/{type}?lat={lat}&lon={lon}&appid={self.__get_appid()}&units={units}&lang={lang}{count}")
             return r.json()
-        
+
         @cache_data(ttl=ttl, show_spinner=f"Loading {type} weather forecast for {zip}...")
         def cityid(id: str):
-            r = self.session.get(f"{ENDPOINT}/forecast/{type}?id={id}&appid={self.__get_appid()}&units={units}&lang={lang}{count}")
+            r = self.session.get(
+                f"{ENDPOINT}/forecast/{type}?id={id}&appid={self.__get_appid()}&units={units}&lang={lang}{count}")
             return r.json()
-        
+
         @cache_data(ttl=ttl, show_spinner=f"Loading {type} weather forecast for {zip}...")
         def zipcode(zip: str):
-            r = self.session.get(f"{ENDPOINT}/forecast/{type}?zip={zip}&appid={self.__get_appid()}&units={units}&lang={lang}{count}")
+            r = self.session.get(
+                f"{ENDPOINT}/forecast/{type}?zip={zip}&appid={self.__get_appid()}&units={units}&lang={lang}{count}")
             return r.json()
 
         if q is not None:
@@ -127,7 +137,7 @@ class OpenWeatherMapConnection(ExperimentalBaseConnection[Session]):
             return zipcode(zip=zip)
         else:
             return None
-        
+
     # Utility function to get the icon URL
     def get_icon_url(id: str) -> str:
         return f"https://openweathermap.org/img/wn/{id}@2x.png"
--- demo/streamlit.py
+++ demo/streamlit.py
@@ -11,7 +11,11 @@ st.title("🌤️ OpenWeatherMap API Explorer")
 
 # OpenWeatherMap API connection
 conn = st.experimental_connection('owm', type=OpenWeatherMapConnection)
+
 # Get the connection session using conn.session attribute
+# Get the connection units using conn.units attribute (default to standard)
+# Get the connection language using conn.lang attribute (default to en)
+
 
 col1, col2 = st.columns(2)
 
@@ -19,7 +23,8 @@ with col1:
     st.subheader('Get the current weather')
     with st.form("weather_by_city"):
         st.markdown("**Search by city**")
-        query = st.text_input("Enter the city", value="", key='q', type="default", help=None, placeholder="City, Country", label_visibility="visible")        
+        query = st.text_input("Enter the city", value="", key='q', type="default",
+                              help=None, placeholder="City, Country", label_visibility="visible")
 
         submitted = st.form_submit_button("Get weather for city")
         if submitted:
@@ -30,9 +35,11 @@ with col1:
         st.markdown("**Search by coordinates**")
         lat, lon = st.columns(2)
         with lat:
-            latitude = st.number_input("Enter the latitude", key='lat', min_value=-90.00, max_value=90.00, value=0.00, label_visibility="visible")
+            latitude = st.number_input("Enter the latitude", key='lat', min_value=-
+                                       90.00, max_value=90.00, value=0.00, label_visibility="visible")
         with lon:
-            longitude = st.number_input("Enter the longitude", key='lon', min_value=-180.00, max_value=179.99, value=0.00, label_visibility="visible")
+            longitude = st.number_input("Enter the longitude", key='lon', min_value=-
+                                        180.00, max_value=179.99, value=0.00, label_visibility="visible")
 
         submitted = st.form_submit_button("Get weather for coordinates")
         if submitted:
