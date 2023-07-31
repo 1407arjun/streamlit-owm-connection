8d21a440a78ce1a2ff6b8fc3f2d71011f9756e39
Arjun Sivaraman <arjun140702@gmail.com>
2023-07-30 22:43:52 -0700

    Implement initial connection

--- st_owm_connection/connection.py
+++ st_owm_connection/connection.py
@@ -1,7 +1,15 @@
 import requests
+from requests.sessions import Session
 from streamlit.connections import ExperimentalBaseConnection
 from streamlit.runtime.caching import cache_data
 
-class OpenWeatherMapConnection():
-    def _connect():
-        return
+class OpenWeatherMapConnection(ExperimentalBaseConnection[Session]):
+    def _connect(self) -> Session:
+        secrets = self._secrets.to_dict()
+        self.appid = secrets.pop("appid", None)
+        self._session = requests.Session()
+        return self.session
+    
+    @property
+    def session(self) -> Session:
+        return self._session
