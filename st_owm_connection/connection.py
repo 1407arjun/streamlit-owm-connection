22492b33642b7ae88435d3223d1988220c87cc32
Arjun Sivaraman <arjun140702@gmail.com>
2023-07-30 23:25:30 -0700

    Implement current weather by latitude and longitude

--- st_owm_connection/connection.py
+++ st_owm_connection/connection.py
@@ -3,13 +3,37 @@ from requests.sessions import Session
 from streamlit.connections import ExperimentalBaseConnection
 from streamlit.runtime.caching import cache_data
 
+from typing import Any, Union, Optional, overload
+from datetime import timedelta
+
+BASE_URL = "https://api.openweathermap.org/data/2.5"
+
 class OpenWeatherMapConnection(ExperimentalBaseConnection[Session]):
-    def _connect(self) -> Session:
-        secrets = self._secrets.to_dict()
-        self.appid = secrets.pop("appid", None)
-        self._session = requests.Session()
+    def _connect(self, **kwargs) -> Session:
+        if 'appid' in kwargs:
+            self.appid = kwargs.pop('appid')
+        
+        if self.appid is None:
+            secrets = self._secrets.to_dict()
+
+            if secrets.get("appid") == None:
+                raise ValueError("AppID/API Key not provided in secrets")
+            else:
+                self.appid = secrets.pop("appid", None)
+        
+        self.session = requests.Session()
         return self.session
     
-    @property
     def session(self) -> Session:
-        return self._session
+        return self.session
+    
+    @overload
+    def current(self, name: str, ttl: Optional[Union[float, int, timedelta]] = None):
+        pass
+
+    def current(self, lat: float, lon: float, ttl: Optional[Union[float, int, timedelta]] = None) -> Any:
+        @cache_data(ttl=ttl, show_spinner="Loading city weather...")
+        def weather_by_city(lat: float, lon: float):
+            r = self.session.get(f"{BASE_URL}/forecast?lat={lat}&lon={lon}&appid={self.appid}")
+            return r.json()
+        return weather_by_city(lat, lon)
