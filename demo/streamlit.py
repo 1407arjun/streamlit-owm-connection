7145948663360a40ee1d9540e455898e976ccdfd
Arjun Sivaraman <arjun140702@gmail.com>
2023-07-31 14:28:05 -0700

    Modify layouts to 2-columns

--- demo/streamlit.py
+++ demo/streamlit.py
@@ -3,28 +3,38 @@ from st_owm_connection import OpenWeatherMapConnection
 
 st.set_page_config(
     page_title='OpenWeatherMap API Explorer',
-    page_icon='🌤️'
+    page_icon='🌤️',
+    layout="wide"
 )
 
 st.title("🌤️ OpenWeatherMap API Explorer")
 
 # OpenWeatherMap API connection
 conn = st.experimental_connection('owm', type=OpenWeatherMapConnection)
-
-with st.form("weather"):
-    st.subheader('Get the current weather')
-    st.markdown("**Search by city**")
-    query = st.text_input("Enter the city", value="", key='q', type="default", help=None, placeholder="City, Country", label_visibility="visible")
-
-    st.divider()
-    
-    st.markdown("**Search by latitude and longitude**")
-    latitude = st.number_input("Enter the latitude", key='lat', min_value=-90.00, max_value=90.00, value=0.00, label_visibility="visible")
-    longitude = st.number_input("Enter the longitude", key='lon', min_value=-180.00, max_value=179.99, value=0.00, label_visibility="visible")
-
-    submitted = st.form_submit_button("Submit")
-
-
 # Get the connection session using conn.session attribute
 
-st.json(conn.weather(latitude, longitude))
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader('Get the current weather')
+    with st.form("weather_by_city"):
+        st.markdown("**Search by city**")
+        query = st.text_input("Enter the city", value="", key='q', type="default", help=None, placeholder="City, Country", label_visibility="visible")        
+
+        submitted = st.form_submit_button("Get weather for city")
+        if submitted:
+            with col2:
+                st.json(conn.current(query))
+
+    with st.form("weather_by_latlon"):
+        st.markdown("**Search by coordinates**")
+        lat, lon = st.columns(2)
+        with lat:
+            latitude = st.number_input("Enter the latitude", key='lat', min_value=-90.00, max_value=90.00, value=0.00, label_visibility="visible")
+        with lon:
+            longitude = st.number_input("Enter the longitude", key='lon', min_value=-180.00, max_value=179.99, value=0.00, label_visibility="visible")
+
+        submitted = st.form_submit_button("Get weather for coordinates")
+        if submitted:
+            with col2:
+                st.json(conn.current(latitude, longitude))
